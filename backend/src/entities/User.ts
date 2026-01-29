76325f47bfbb4dd054ad3ff9f910eb9709bd95d0
on4r4p <on4r4p@disrot.org>
2026-01-29 12:53:59 +0100

    initial template

---
+++ backend/src/entities/User.ts
@@ -0,0 +1,71 @@
+import { IsEmail, IsStrongPassword } from "class-validator";
+import { Field, InputType, Int, ObjectType } from "type-graphql";
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+
+export enum UserRole {
+  Admin = "admin",
+  Visitor = "visitor",
+}
+
+@ObjectType()
+@Entity()
+export class User extends BaseEntity {
+  @Field(() => Int)
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field()
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  hashedPassword: string;
+
+  @Field()
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Field()
+  @Column({ enum: UserRole, default: UserRole.Visitor })
+  role: UserRole;
+}
+
+@InputType()
+export class SignupInput {
+  @Field()
+  @IsEmail({}, { message: "L'email doit être valide" })
+  email: string;
+
+  @Field()
+  @IsStrongPassword(
+    {},
+    {
+      message:
+        "Le mot de passe doit contenir au moins 8 caractères, dont une minuscule, une majuscule, un chiffre et un caractère spécial",
+    },
+  )
+  password: string;
+}
+
+@InputType()
+export class LoginInput {
+  @Field()
+  @IsEmail({}, { message: "L'email doit être valide" })
+  email: string;
+
+  @Field()
+  @IsStrongPassword(
+    {},
+    {
+      message:
+        "Le mot de passe doit contenir au moins 8 caractères, dont une minuscule, une majuscule, un chiffre et un caractère spécial",
+    },
+  )
+  password: string;
+}
