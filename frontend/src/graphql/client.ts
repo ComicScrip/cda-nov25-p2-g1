ddcb8618cf1efbcebaf6ac6c579f61b6454373d0
User <p.o.maisonneuve@gmail.com>
2026-02-03 12:01:19 +0100

    fixing default uri api link from the graphql client

--- frontend/src/graphql/client.ts
+++ frontend/src/graphql/client.ts
@@ -2,14 +2,12 @@ import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
 const uri = process.env.NEXT_PUBLIC_GRAPHQL_API_URL;
 
-if (!uri) throw new Error("missing API URL, check your .env !");
-
 console.log("GQL API URL :", uri);
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
-    uri,
+    uri: uri || "/graphql",
     credentials: "include", // Important: Include cookies with requests
   }),
 });
